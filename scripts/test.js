31d4c3d69cc975399e74c5ff6fe6261abff4b564
Shy0for0u <pav.tro2014@yandex.ru>
2022-11-29 22:36:14 +0300

    first commit

---
+++ scripts/test.js
@@ -0,0 +1,13 @@
+﻿
+function foo() {
+    var j = 0;
+    for (let i = j; i < 10; i++) {
+        console.log(i);
+    }
+    console.log("after for");
+    console.log(i);
+}
+
+
+foo();
+
---
+++ scripts/test2.js
@@ -0,0 +1,7 @@
+﻿function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(3, 5));
+console.log(multiply(20, 20));
+console.log(multiply(3, 0.5));
